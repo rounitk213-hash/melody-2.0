3e4e03f6e145f49f8fe9d768f495b112dc3620c0
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2025-11-06 15:57:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,6 +86,10 @@ STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
 
+AMBOT = [
+    "💞", "🦋", "🔍", "🧪", "💖", "⚡️", "🔥", "❤️‍🔥", "🎩", "🌈", "🍷", "🥂", "👀", "🥃", "🥤", "🕊️", 
+    "💔", "💘", "🎉", "🥀", "🌹", "❣️", "❤️", "🦋", "🪄", "💌", "💗", "💝", "🧨"
+]
 
 BANNED_USERS = filters.user()
 adminlist = {}
